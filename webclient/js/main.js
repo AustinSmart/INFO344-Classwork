bee3467c167fe001c0f094b968eb4732c029eb18
Austin Smart <adsmart6@uw.edu>
2017-03-30 16:49:19 -0700

    web client done, added dockerfile

---
+++ webclient/js/main.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const API = "http://138.197.219.125/v1/summary?url=";
+
+function search() {
+    var input = document.getElementById("input").value;
+    var table = document.getElementById("table");
+    var title = document.getElementById("title")
+    var description = document.getElementById("description")
+    var image = document.createElement("img");
+    var imageDiv = document.getElementById("image-div");
+    var results = document.getElementById("results");
+
+
+    fetch(API + input)
+        .then(function (resp) {
+            return resp.json()
+        }).then(function (data) {
+            results.style.display = "inherit"
+            title.innerHTML = data.title;
+            description.innerHTML = data.description;
+            image.src = data.image;
+            imageDiv.appendChild(image);
+            console.log(data)
+        }).catch(function (err) {
+            console.error(err)
+        });
+}

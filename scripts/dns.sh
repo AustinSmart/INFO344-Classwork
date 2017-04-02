86afc19f060b37999681a34f77739672bdf91dac
Austin Smart <adsmart6@uw.edu>
2017-04-02 11:51:08 -0700

    added get dns records script

---
+++ scripts/dns.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# $1: Record type
+
+# View DNS Records for austinsmart.com, returns all records if no type is supplied
+
+zone=$(curl -X GET "https://api.cloudflare.com/client/v4/zones?name=austinsmart.com&status=active&page=1&per_page=20&order=status&direction=desc&match=all" \
+     -H "X-Auth-Email: 4.smart.austin@gmail.com" \
+     -H "X-Auth-Key: $CFTOKEN" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+# Get all
+if ([ -z "$1" ]) then
+dns=$(curl -X GET "https://api.cloudflare.com/client/v4/zones/$zone/dns_records?page=1&per_page=20&order=type&direction=desc&match=all" \
+     -H "X-Auth-Email: 4.smart.austin@gmail.com" \
+     -H "X-Auth-Key: $CFTOKEN" \
+     -H "Content-Type: application/json" | jq -r .result)
+else
+# Get type
+dns=$(curl -X GET "https://api.cloudflare.com/client/v4/zones/$zone/dns_records?type=$1&page=1&per_page=20&order=type&direction=desc&match=all" \
+     -H "X-Auth-Email: 4.smart.austin@gmail.com" \
+     -H "X-Auth-Key: $CFTOKEN" \
+     -H "Content-Type: application/json" | jq -r .result)
+fi
+
+# Print
+for i in "${dns[@]}"
+do
+   echo "$i"
+done
--- scripts/remove_droplets_from_load_balancer.sh
+++ scripts/remove_droplets_from_load_balancer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Get all droplets in load balancer
 droplet_ids=$(sh ./get_load_balancer.sh | jq -r '.load_balancers[0].droplet_ids')
 echo Droplets: ${droplet_ids}
 
@@ -9,6 +10,7 @@ read droplet
 echo "Do you want to destroy it as well? (y/n)"
 read destroy
 
+# Removed selected droplet
 if([ "$droplet" != "" ]) then
     curl -X DELETE "https://api.digitalocean.com/v2/load_balancers/dfc8c5be-a4ae-4fdb-9e70-6c4a11999e59/droplets" \
         -H "Authorization: Bearer $DOTOKEN" \
@@ -18,6 +20,7 @@ else
     echo Please enter a droplet id
 fi    
 
+# Destroy droplet if 'y'
 if([ "$destroy" == "y" ]  || [ "$destroy" == "yes" ]) then
     curl -X DELETE "https://api.digitalocean.com/v2/droplets/$droplet" \
         -H "Authorization: Bearer $DOTOKEN" \

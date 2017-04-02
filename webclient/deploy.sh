a8ba4d2b65209ff9fef3dedc62abdb040350f7ee
Austin Smart <adsmart6@uw.edu>
2017-04-01 18:27:24 -0700

    added webclient deploy script

---
+++ webclient/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Build the docker container
+docker build -t austinsmart/info344webclient . --no-cache
+
+# Push the docker container
+docker push austinsmart/info344webclient:latest
+
+# SSH. root = bad
+ssh root@138.68.41.64 ' 
+docker stop webclient
+docker rm webclient
+docker pull austinsmart/info344webclient:latest
+docker run --name webclient -p 443:443 -p 80:80 -v /etc/info344.austinsmart.com.pem:/etc/info344.austinsmart.com.pem -v /etc/info344.austinsmart.com.key:/etc/info344.austinsmart.com.key -dt austinsmart/info344webclient
+docker ps
+'

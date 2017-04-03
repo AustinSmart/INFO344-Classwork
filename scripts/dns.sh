1996fef5329a719fc597409805b6334f5af429db
Austin Smart <adsmart6@uw.edu>
2017-04-02 20:51:12 -0700

    removed...pii stuff

--- scripts/dns.sh
+++ scripts/dns.sh
@@ -5,20 +5,20 @@
 # View DNS Records for austinsmart.com, returns all records if no type is supplied
 
 zone=$(curl -sS -X GET "https://api.cloudflare.com/client/v4/zones?name=austinsmart.com&status=active&page=1&per_page=20&order=status&direction=desc&match=all" \
-     -H "X-Auth-Email: 4.smart.austin@gmail.com" \
+     -H "X-Auth-Email: $CFEMAIL" \
      -H "X-Auth-Key: $CFTOKEN" \
      -H "Content-Type: application/json" | jq -r .result[0].id)
 
 # Get all
 if ([ -z "$1" ]) then
 dns=$(curl -sS -X GET "https://api.cloudflare.com/client/v4/zones/$zone/dns_records?page=1&per_page=20&order=type&direction=desc&match=all" \
-     -H "X-Auth-Email: 4.smart.austin@gmail.com" \
+     -H "X-Auth-Email: $CFEMAIL" \
      -H "X-Auth-Key: $CFTOKEN" \
      -H "Content-Type: application/json" | jq -r .result)
 else
 # Get type
 dns=$(curl -sS -X GET "https://api.cloudflare.com/client/v4/zones/$zone/dns_records?type=$1&page=1&per_page=20&order=type&direction=desc&match=all" \
-     -H "X-Auth-Email: 4.smart.austin@gmail.com" \
+     -H "X-Auth-Email: $CFEMAIL" \
      -H "X-Auth-Key: $CFTOKEN" \
      -H "Content-Type: application/json" | jq -r .result)
 fi
--- scripts/verify_ssl.sh
+++ scripts/verify_ssl.sh
@@ -2,13 +2,13 @@
 
 # get primary zone
 zone=$(curl -sS -X GET "https://api.cloudflare.com/client/v4/zones?name=austinsmart.com&status=active&page=1&per_page=20&order=status&direction=desc&match=all" \
-     -H "X-Auth-Email: 4.smart.austin@gmail.com" \
+     -H "X-Auth-Email: $CFEMAIL" \
      -H "X-Auth-Key: $CFTOKEN" \
      -H "Content-Type: application/json" | jq -r .result[0].id)
 
 # Verify SSL for zone
 result=$(curl -sS -X GET "https://api.cloudflare.com/client/v4/zones/$zone/ssl/verification" \
-     -H "X-Auth-Email: 4.smart.austin@gmail.com" \
+     -H "X-Auth-Email: $CFEMAIL" \
      -H "X-Auth-Key: $CFTOKEN" \
      -H "Content-Type: application/json" | jq -r .result)
 
